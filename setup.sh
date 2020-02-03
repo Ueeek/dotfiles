154a15517c5da30c9e13415376283cb7d36fa49f
Ueeek <ueki.k.ae@m.titech.ac.jp>
2020-02-03 17:22:49 +0900

    lint

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-DOT_FILES=(.zshrc .zsh-history .zshenv .zsh-update .tmux.conf .gitconfig)
+DOT_FILES=(.zshrc .zsh-history .zshenv .zsh-update .tmux.conf .gitconfig .tern-project .eslintrc)
 
 for file in ${DOT_FILES[@]}
 do

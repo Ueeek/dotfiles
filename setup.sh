b4ce146898ed27d1855661d42131fd55c60426b0
Ueeek <ueki@UekinoMacBook-Air.local>
2020-07-20 16:27:54 +0900

    fix name

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-DOT_FILES=(.zshrc .zsh-history .zshenv .zsh-update .tmux.conf .gitconfig .tern-project .eslintrc)
+DOT_FILES=(.zshrc .zsh-history .zshenv .zsh-update .tmux.conf .gitconfig)
 #symbolic
 for file in ${DOT_FILES[@]}
 do
